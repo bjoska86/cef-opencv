9c5c03d7f54ba0e88b995ce89cf506c814eb1b23
bjoska86 <bjozsef01@gmail.com>
2021-07-02 19:07:58 +0200

    Basic opencv camera feed

--- cef-opencv/cef-opencv.cpp
+++ cef-opencv/cef-opencv.cpp
@@ -1,23 +1,53 @@
-// cef-opencv.cpp : This file contains the 'main' function. Program execution
-// begins and ends there.
-//
-
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // Open the default video camera
+  VideoCapture cap(0);
+
+  // if not success, exit program
+  if (cap.isOpened() == false) {
+    cout << "Cannot open the video camera" << endl;
+    cin.get();  // wait for any key press
+    return -1;
+  }
+
+  double dWidth =
+      cap.get(CAP_PROP_FRAME_WIDTH);  // get the width of frames of the video
+  double dHeight =
+      cap.get(CAP_PROP_FRAME_HEIGHT);  // get the height of frames of the video
+
+  cout << "Resolution of the video : " << dWidth << " x " << dHeight << endl;
+
+  string window_name = "My Camera Feed";
+  namedWindow(window_name);  // create a window called "My Camera Feed"
+
+  while (true) {
+    Mat frame;
+    bool bSuccess = cap.read(frame);  // read a new frame from video
+
+    // Breaking the while loop if the frames cannot be captured
+    if (bSuccess == false) {
+      cout << "Video camera is disconnected" << endl;
+      cin.get();  // Wait for any key press
+      break;
+    }
+
+    // show the frame in the created window
+    imshow(window_name, frame);
+
+    // wait for for 10 ms until any key is pressed.
+    // If the 'Esc' key is pressed, break the while loop.
+    // If the any other key is pressed, continue the loop
+    // If any key is not pressed withing 10 ms, continue the loop
+    if (waitKey(10) == 27) {
+      cout << "Esc key is pressed by user. Stoppig the video" << endl;
+      break;
+    }
+  }
+
   return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add
-//   Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project
-//   and select the .sln file
